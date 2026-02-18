1c0352f370a2020b101fede3d250f41570063fec
sachitpat123-source <sachitpat123@gmail.com>
2026-02-18 13:55:10 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,19 @@
 
+from machine import Pin
+import time
+
+dhruv = Pin(23, Pin.OUT)
+ansh = Pin(22, Pin.OUT)
+tanay = Pin(19, Pin.OUT)
+avyukt = Pin(18, Pin.OUT)
+x=[[0,1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]]
+
+while True:
+    for i in x:
+        dhruv.value(i[0])
+        ansh.value(i[1])
+        tanay.value(i[2])
+        avyukt.value(i[3])
+        time.sleep(0.003)
+        
+
